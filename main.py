f114f37f29039874bfe30aff7e95067643e0c64d
Mridul-CODE05 <mridulbhardwaj888@gmail.com>
2026-06-11 16:38:30 +0530

    expenses are now stored

--- main.py
+++ main.py
@@ -10,10 +10,15 @@ while True:
     choice = input("Enter your choice: ")
     print()
 
+
+    expenses = []  # This will hold the list of expenses
+    expense = {} # This will hold the details of a single expense
     if choice == '1':
         name = input("Enter expense name: ")
         currency = "₹"
         amount = float(input(f"Enter expense amount: {currency}"))
+        expense = {"name": name, "currency": currency, "amount": amount}
+        expenses.append(expense)
         print(f"Expense '{name}' of amount {currency}{amount} added successfully!")
     elif choice == '4':
         print("Exiting the Expense Tracker. Goodbye!")
